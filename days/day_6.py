379f5463d39efe655f5dd89c504160af162e5aca
DaveyJH <daveyj.dh@googlemail.com>
2022-12-07 13:48:48 +0000

    feat: Add day 6 solution

---
+++ days/day_6.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import re
+from aoc_data import check_for_data, data_file
+
+DAY = re.search(r"(\d+)\.py", __file__).group(1)
+check_for_data()
+print(f"Day {DAY}:\n")
+
+with open(data_file()) as f:
+    content = f.read()
+    marker_count = 4
+    while True:
+        if len(set(content[marker_count - 4:marker_count])) == 4:
+            break
+        marker_count += 1
+    print("  Part 1: ", marker_count)
+    message_count = 14
+    while True:
+        if len(set(content[message_count - 14:message_count])) == 14:
+            break
+        message_count += 1
+    print("  Part 2: ", message_count)
+
+print()
