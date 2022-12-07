26de16c574df248549af920ecd433acfffc166ca
DaveyJH <daveyj.dh@googlemail.com>
2022-12-07 02:24:15 +0000

    style: Add doc strings

--- aoc_data/__init__.py
+++ aoc_data/__init__.py
@@ -10,6 +10,7 @@ FILE_REGEX = re.compile(r"(\d+)\.py$")
 
 
 def data_file():
+    """Provides data file path for day specified in filename"""
     DATA_FILE = "aoc_data/day_{}.txt"
     return DATA_FILE.format(
         FILE_REGEX.search(inspect.stack()[1].filename).group(1)
@@ -17,11 +18,13 @@ def data_file():
 
 
 def today_url(call):
+    """Provides input data URL for day specified in filename"""
     URL = "https://adventofcode.com/2022/day/{}/input"
     return URL.format(FILE_REGEX.search(call).group(1))
 
 
 def retrieve_data(day_url):
+    """Retrieves data from AoC"""
     SESSION = os.environ["session"]
     response = requests.get(
         url=day_url,
@@ -34,6 +37,7 @@ def retrieve_data(day_url):
 
 
 def check_for_data():
+    """Creates input data file for day if it does not already exists"""
     calling_filename = inspect.stack()[1].filename
     file = f"aoc_data/day_{FILE_REGEX.search(calling_filename).group(1)}.txt"
     if not os.path.exists(file):
