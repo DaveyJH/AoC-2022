c8aa51890ba3c7294f7a6b5fbff44d176b3df241
DaveyJH <daveyj.dh@googlemail.com>
2022-12-07 04:00:17 +0000

    feat: Add day 4 solution

---
+++ days/day_4.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import re
+from aoc_data import check_for_data, data_file
+
+DAY = re.search(r"(\d+)\.py", __file__).group(1)
+check_for_data()
+print(f"Day {DAY}:\n")
+
+with open(data_file()) as f:
+    content = f.readlines()
+    FIRST = re.compile(r"^(\d+-\d+)")
+    SECOND = re.compile(r",(\d+-\d+)$")
+    NUMS = re.compile(r"(\d+)-(\d+)")
+    total = 0
+    revised_total = 0
+    for line in content:
+        first = FIRST.match(line).group(1)
+        second = SECOND.search(line).group(1)
+        first_low, first_high = NUMS.match(first).groups()
+        second_low, second_high = NUMS.match(second).groups()
+        if (
+            (
+                int(first_low) <= int(second_low)
+                and int(first_high) >= int(second_high)
+            )
+            or (
+                int(second_low) <= int(first_low)
+                and int(second_high) >= int(first_high)
+            )
+        ):
+            total += 1
+        if (
+            (
+                int(first_low) <= int(second_low)
+                and int(first_high) >= int(second_low)
+            )
+            or (
+                int(second_low) <= int(first_low)
+                and int(second_high) >= int(first_low)
+            )
+        ):
+            revised_total += 1
+    print("  Part 1: ", total)
+    print("  Part 2: ", revised_total)
+
+print()
