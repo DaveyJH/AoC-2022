84680af64b1f61f128137514307b7c38aa4b6466
DaveyJH <daveyj.dh@googlemail.com>
2023-01-28 21:23:24 +0000

    fix: Repeat directory names not overwritten

--- days/day_7.py
+++ days/day_7.py
@@ -40,14 +40,16 @@ with open(data_file()) as f:
     content = f.readlines()
     directories = {"/": Directory("/"), }  # create root
     cd = directories["/"]  # start at root
+    part_1_size = 0
     for line in content:
         if cd_reg := CD_CMD.match(line):
             if cd_reg.group(1) == "/":
                 cd = directories["/"]  # return to root
             elif (name := cd_reg.group(1)) != "..":
-                directories[name] = cd.dirs[name]  # add directory to dict
-                cd = directories[name]  # cd to relevant directory
+                cd = cd.dirs[name]  # cd to relevant directory
             else:
+                if (size := cd.calc_size()) <= 100000:
+                    part_1_size += size
                 cd = cd.parent  # `cd ..` to parent
         elif dir_reg := DIR_REGEX.match(line):
             dir = dir_reg.group(1)
@@ -55,9 +57,7 @@ with open(data_file()) as f:
         elif file_reg := FILE_REGEX.match(line):
             f_size, f_name = file_reg.group(1, 2)
             cd.files[f_name] = File(f_name, f_size)  # add file w/size to cd
+    if (size := cd.calc_size()) <= 100000:  # needed to check final cd
+        part_1_size += size
 
-
-print("  Part 1: ", sum([
-    d.calc_size() for d in directories.values()
-    if d.calc_size() <= 100000
-]))
+print(part_1_size)
